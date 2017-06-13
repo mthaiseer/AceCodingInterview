71809cde0055685f7725514785fde96211c8c05c
mthaiseer <mohamed.thaiseer@gmail.com>
2017-06-13 22:02:24 +0530

    comment added

--- StringsAndArrays/src/common/StringUniqueCharacterChecker.java
+++ StringsAndArrays/src/common/StringUniqueCharacterChecker.java
@@ -3,7 +3,7 @@ package common;
 public class StringUniqueCharacterChecker {
 
 	public static void main(String[] args) {
-		
+
 		String text = "abcdefa";
 		System.out.println(isCharactersUniqueON2(text));
 
@@ -12,8 +12,8 @@ public class StringUniqueCharacterChecker {
 	/**
 	 * Time complexity : O(N2) Space complexity : O(1)
 	 * 
-	 * @param text 
-	 * @return boolean 
+	 * @param text
+	 * @return boolean
 	 */
 	public static boolean isCharactersUniqueON2(String text) {
 
@@ -24,21 +24,17 @@ public class StringUniqueCharacterChecker {
 		char[] textArray = text.toCharArray();
 
 		for (int i = 0; i < textArray.length; i++) {
-
 			for (int j = 0; j < textArray.length; j++) {
-
+				//if both index are same, ignore it
 				if (i == j) {
 					continue;
 				}
-
+				//if identical characters are found, return false. 
 				if (textArray[i] == textArray[j]) {
 					return false;
-				}
-
+				} 
 			}
 		}
-
 		return true;
 	}
-
 }
