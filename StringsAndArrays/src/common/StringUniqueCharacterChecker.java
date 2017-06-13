d3cfc4fb9346f4ba6e9212398b5e04a9cb359d71
mthaiseer <mohamed.thaiseer@gmail.com>
2017-06-13 21:53:41 +0530

    commit

---
+++ StringsAndArrays/src/common/StringUniqueCharacterChecker.java
@@ -0,0 +1,44 @@
+package common;
+
+public class StringUniqueCharacterChecker {
+
+	public static void main(String[] args) {
+		
+		String text = "abcdefa";
+		System.out.println(isCharactersUniqueON2(text));
+
+	}
+
+	/**
+	 * Time complexity : O(N2) Space complexity : O(1)
+	 * 
+	 * @param text
+	 * @return
+	 */
+	public static boolean isCharactersUniqueON2(String text) {
+
+		if (text == null || text.length() == 0) {
+			return false;
+		}
+
+		char[] textArray = text.toCharArray();
+
+		for (int i = 0; i < textArray.length; i++) {
+
+			for (int j = 0; j < textArray.length; j++) {
+
+				if (i == j) {
+					continue;
+				}
+
+				if (textArray[i] == textArray[j]) {
+					return false;
+				}
+
+			}
+		}
+
+		return true;
+	}
+
+}
