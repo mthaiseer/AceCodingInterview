618c4866c90a37c7ada8a04d0410db6c5563ca2b
mthaiseer <mohamed.thaiseer@gmail.com>
2017-06-13 22:32:27 +0530

    completed isUnique

--- StringsAndArrays/src/common/StringUniqueCharacterChecker.java
+++ StringsAndArrays/src/common/StringUniqueCharacterChecker.java
@@ -1,11 +1,24 @@
 package common;
 
+import java.util.Arrays;
+
+/**
+ * This class used to find a String contains unique characters or not
+ * if String contains unique characters returns true, else return false.
+ * 
+ * there are 3 versions of implementation are available in this class 
+ * 
+ * @author mohamed.thaiseer
+ *
+ */
 public class StringUniqueCharacterChecker {
 
 	public static void main(String[] args) {
 
-		String text = "abcdefa";
+		String text = "abcxyza";
 		System.out.println(isCharactersUniqueON2(text));
+		System.out.println(isUniqueCharBetterSolution(text));
+		System.out.println(isUniqueCharWithSorting(text));
 
 	}
 
@@ -25,16 +38,70 @@ public class StringUniqueCharacterChecker {
 
 		for (int i = 0; i < textArray.length; i++) {
 			for (int j = 0; j < textArray.length; j++) {
-				//if both index are same, ignore it
+				// if both index are same, ignore it
 				if (i == j) {
 					continue;
 				}
-				//if identical characters are found, return false. 
+				// if identical characters are found, return false.
 				if (textArray[i] == textArray[j]) {
 					return false;
-				} 
+				}
 			}
 		}
+		return true;
+	}
+
+	/**
+	 * Time complexity : O(N)
+	 * 
+	 * @param inputText
+	 * @return
+	 */
+	public static boolean isUniqueCharBetterSolution(String inputText) {
+
+		if (inputText == null || inputText.length() == 0) {
+			return false;
+		}
+
+		if (inputText.length() > 128) {
+			return false;
+		}
+
+		int[] charArray = new int[128];
+
+		for (int i = 0; i < inputText.length(); i++) {
+			int charIndex = inputText.charAt(i);
+			if (charArray[charIndex] != 0) {
+				return false;
+			}
+			charArray[charIndex] = charIndex;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Time complexity O(N)
+	 * @param inputText
+	 * @return
+	 */
+	public static boolean isUniqueCharWithSorting(String inputText) {
+		if (inputText == null || inputText.length() == 0) {
+			return false;
+		}
+		if (inputText.length() > 128) {
+			return false;
+		}
+		char[] charArray = inputText.toCharArray();
+		Arrays.sort(charArray);
+
+		for (int i = 0; i < charArray.length - 1; i++) {
+
+			if (charArray[i] == charArray[i + 1]) {
+				return false;
+			}
+		}
+
 		return true;
 	}
 }
